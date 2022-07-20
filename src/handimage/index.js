1a960556020dd8edacdeb55280eaeb528cba176b
JaiJoshi123 <64401010+JaiJoshi123@users.noreply.github.com>
2022-07-20 19:34:06 +0530

    Upload project files

---
+++ src/handimage/index.js
@@ -0,0 +1,302 @@
+import A_sign from './A.svg';
+import B_sign from './B.svg';
+import C_sign from './C.svg';
+import D_sign from './D.svg';
+import E_sign from './E.svg';
+import F_sign from './F.svg';
+import G_sign from './G.svg';
+import H_sign from './H.svg';
+import I_sign from './I.svg';
+import J_sign from './J.svg';
+import K_sign from './K.svg';
+import L_sign from './L.svg';
+import M_sign from './M.svg';
+import N_sign from './N.svg';
+import O_sign from './O.svg';
+import P_sign from './P.svg';
+import Q_sign from './Q.svg';
+import R_sign from './R.svg';
+import S_sign from './S.svg';
+import T_sign from './T.svg';
+import U_sign from './U.svg';
+import V_sign from './V.svg';
+import W_sign from './W.svg';
+import X_sign from './X.svg';
+import Y_sign from './Y.svg';
+import Z_sign from './Z.svg';
+import love_sign from './love.png'
+import thankyou_sign from './thankyou.png'
+import yes_sign from './yes.png'
+// import no_sign from './no.png'
+
+import A_hand from './Ahand.svg';
+import B_hand from './Bhand.svg';
+import C_hand from './Chand.svg';
+import D_hand from './Dhand.svg';
+import E_hand from './Ehand.svg';
+import F_hand from './Fhand.svg';
+import G_hand from './Ghand.svg';
+import H_hand from './Hhand.svg';
+import I_hand from './Ihand.svg';
+import J_hand from './Jhand.svg';
+import K_hand from './Khand.svg';
+import L_hand from './Lhand.svg';
+import M_hand from './Mhand.svg';
+import N_hand from './Nhand.svg';
+import O_hand from './Ohand.svg';
+import P_hand from './Phand.svg';
+import Q_hand from './Qhand.svg';
+import R_hand from './Rhand.svg';
+import S_hand from './Shand.svg';
+import T_hand from './Thand.svg';
+import U_hand from './Uhand.svg';
+import V_hand from './Vhand.svg';
+import W_hand from './Whand.svg';
+import X_hand from './Xhand.svg';
+import Y_hand from './Yhand.svg';
+import Z_hand from './Zhand.svg';
+import Love_hand from './love_hand.png'
+import Thankyou_hand from './thankyou_hand.png'
+import Yes_hand from './yes_hand.png'
+// import No_hand from './no_hand.png'
+ 
+import one_sign from './1.png'
+import two_sign from './2.png'
+import three_sign from './3.png'
+import four_sign from './4.png'
+import five_sign from './5.png'
+import six_sign from './6.png'
+import seven_sign from './7.png'
+import eight_sign from './8.png'
+import nine_sign from './9.png'
+import zero_sign from './0.png'
+
+import one_hand from './1_hand.png'
+import two_hand from './2_hand.png'
+import three_hand from './3_hand.png'
+import four_hand from './4_hand.png'
+import five_hand from './5_hand.png'
+import six_hand from './6_hand.png'
+import seven_hand from './7_hand.png'
+import eight_hand from './8_hand.png'
+import nine_hand from './9_hand.png'
+import zero_hand from './0_hand.png'
+
+
+export var Signimage =  {
+    A: A_sign,
+    B: B_sign,
+    C: C_sign,
+    D: D_sign,
+    E: E_sign,
+    F: F_sign,
+    G: G_sign,
+    H: H_sign,
+    I: I_sign,
+    J: J_sign,
+    K: K_sign,
+    L: L_sign,
+    M: M_sign,
+    N: N_sign,
+    O: O_sign,
+    P: P_sign,
+    Q: Q_sign,
+    R: R_sign,
+    S: S_sign,
+    T: T_sign,
+    U: U_sign,
+    V: V_sign,
+    W: W_sign,
+    X: X_sign,
+    Y: Y_sign,
+    Z: Z_sign
+}
+
+export var Signpass =  [
+    {
+        src: A_hand,
+        alt: 'A'
+    },
+    {
+        src: B_hand,
+        alt: 'B'
+    },
+    {
+        src: C_hand,
+        alt: 'C'
+    },
+    {
+        src: D_hand,
+        alt: 'D'
+    },
+    {
+        src: E_hand,
+        alt: 'E'
+    },
+    {
+        src: F_hand,
+        alt: 'F'
+    },
+    {
+        src: G_hand,
+        alt: 'G'
+    },
+    {
+        src: H_hand,
+        alt: 'H'
+    },
+    {
+        src: I_hand,
+        alt: 'I'
+    },
+    {
+        src: J_hand,
+        alt: 'J'
+    },
+    {
+        src: K_hand,
+        alt: 'K'
+    },
+    {
+        src: L_hand,
+        alt: 'L'
+    },
+    {
+        src: M_hand,
+        alt: 'M'
+    },
+    {
+        src: N_hand,
+        alt: 'N'
+    },
+    {
+        src: O_hand,
+        alt: 'O'
+    },
+    {
+        src: P_hand,
+        alt: 'P'
+    },
+    {
+        src: Q_hand,
+        alt: 'Q'
+    },
+    {
+        src: R_hand,
+        alt: 'R'
+    },
+    {
+        src: S_hand,
+        alt: 'S'
+    },
+    {
+        src: T_hand,
+        alt: 'T'
+    },
+    {
+        src: U_hand,
+        alt: 'U'
+    },
+    {
+        src: V_hand,
+        alt: 'V'
+    },
+    {
+        src: W_hand,
+        alt: 'W'
+    },
+    {
+        src: X_hand,
+        alt: 'X'
+    },
+    {
+        src: Y_hand,
+        alt: 'Y'
+    },
+    {
+        src: Z_hand,
+        alt: 'Z'
+    }
+]
+
+export var WordImage = {
+    Love: love_sign,
+    Thank_You: thankyou_sign,
+    Yes: yes_sign,
+    //No: no_sign
+}
+
+export var Wordpass = [
+    {
+        src: Love_hand,
+        alt: 'Love'
+    },
+    {
+        src: Thankyou_hand,
+        alt: 'Thank_You'
+    },
+    {
+        src: Yes_hand,
+        alt: 'Yes'
+    },
+    // {
+    //     src: No_hand,
+    //     alt: 'No'
+    // }
+]
+    
+export var NumberImage = {
+    One: one_sign,
+    Two: two_sign,
+    Three: three_sign,
+    Four: four_sign,
+    Five: five_sign,
+    Six: six_sign,
+    Seven: seven_sign,
+    Eight: eight_sign,
+    Nine: nine_sign,
+    Zero: zero_sign
+}
+
+export var NumberPass = [
+    {
+        src: one_hand,
+        alt: 'One'
+    },
+    {
+        src: two_hand,
+        alt: 'Two'
+    },
+    {
+        src: three_hand,
+        alt: 'Three'
+    },
+    {
+        src: four_hand,
+        alt: 'Four'
+    },
+    {
+        src: five_hand,
+        alt: 'Five'
+    },
+    {
+        src: six_hand,
+        alt: 'Six'
+    },
+    {
+        src: seven_hand,
+        alt: 'Seven'
+    },
+    {
+        src: eight_hand,
+        alt: 'Eight'
+    },
+    {
+        src: nine_hand,
+        alt: 'Nine'
+    },
+    {
+        src: zero_hand,
+        alt: 'Zero'
+    }
+]
